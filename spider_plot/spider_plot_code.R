f40be1c12067ca66c4beaf4286a2e8de586bb3cd
Norbert Frydrysiak <fantasy22wfry@gmail.com>
2023-11-08 18:39:08 +0100

    spider_plots update

--- spider_plot/spider_plot_code.R
+++ spider_plot/spider_plot_code.R
@@ -1,5 +1,5 @@
 #read csv
-#setwd("/Users/fantasy2fry/Documents/informatyczne/iadstudia/twd/fast_food_data_analysis_project/spider_plot")
+setwd("/Users/fantasy2fry/Documents/informatyczne/iadstudia/twd/fast_food_data_analysis_project/spider_plot")
 df_fast_food <- read.csv("../data/fastfood.csv")
 df_restaurants <- read.csv("../data/Fast_Food_Restaurants_US.csv")
 
@@ -7,6 +7,9 @@ library(fmsb)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+# install.packages("devtools")
+# devtools::install_github("ricardo-bion/ggradar")
+#library(ggradar)
 
 logos_links=c('https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Arby%27s_logo.svg/640px-Arby%27s_logo.svg.png',
               'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Burger_King_logo_%281999%29.svg/2024px-Burger_King_logo_%281999%29.svg.png',
@@ -36,6 +39,25 @@ df_restaurants_prepaired_for_spider_plot=
          average_sugar=100*average_sugar/max(average_sugar),
          average_protein=100*average_protein/max(average_protein))
 
+df_max_min_for_spider_plot=
+  df_restaurants_prepaired_for_spider_plot %>%
+  summarise(max_calories=max(average_calories),
+            max_total_fat=max(average_total_fat),
+            max_cholesterol=max(average_cholesterol),
+            max_carbohydrates=max(average_carbohydrates),
+            max_sugar=max(average_sugar),
+            max_protein=max(average_protein),
+            min_calories=min(average_calories),
+            min_total_fat=min(average_total_fat),
+            min_cholesterol=min(average_cholesterol),
+            min_carbohydrates=min(average_carbohydrates),
+            min_sugar=min(average_sugar),
+            min_protein=min(average_protein))
+df_max_min_for_spider_plot=as.data.frame(t(df_max_min_for_spider_plot))
+df_max_min_for_spider_plot=rbind(df_max_min_for_spider_plot[1:6,],
+                                 df_max_min_for_spider_plot[7:12,])
+
+#########################
 #create spider plot for every restaurant
 df=df_restaurants_prepaired_for_spider_plot[1,2:7]
 name=df_restaurants_prepaired_for_spider_plot[1,1]
@@ -128,3 +150,43 @@ plot+
     plot.title=element_text(face="bold", hjust=0.5, size=18, vjust=-2),
     plot.subtitle=element_text(hjust=0.5, vjust=-5),
   )
+
+
+
+#####################
+# true radar plot
+# for every restaurant
+
+
+
+for (i in 1:8) {
+  df=df_restaurants_prepaired_for_spider_plot[i,2:7]
+  name=df_restaurants_prepaired_for_spider_plot[i,1]
+  df=as.data.frame(t(df))
+  rownames=row.names(df)
+  df=cbind(rownames,df)
+  colnames(df)=c("name","value")
+  df=pivot_wider(df,names_from = name,values_from = value)
+  colnames(df_max_min_for_spider_plot)=colnames(df)
+  df=rbind(df_max_min_for_spider_plot ,df)
+  #radar plot
+  png(paste("radar_plot_",name,".png",sep=""))
+  radarchart(df, axistype=1 , 
+             pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , plty=1, 
+             cglcol="grey", cglty=2, axislabcol="white", cglwd=2,vlcex=0.8 )+
+    title(paste("Stats for",name[[1]] ,"brand"))
+  dev.off()
+  }
+
+
+
+
+
+
+ggplot(df, aes(x=name,y=value))+
+  coord_polar()+
+  theme_minimal()+
+  geom_point()+geom_line(aes(group=1),linetype="solid",color="blue")+
+  labs(title=name)+
+  theme(plot.title = element_text(hjust = 0.5))+
+  ylim(0, 100)
